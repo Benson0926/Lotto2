b4fdcf8b05f0f467238842f59c63df8c314a057f
BensonWu <wu2301292@gmail.com>
2024-03-06 18:03:45 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/csim/benson/lotto2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/benson/lotto2/MainActivity.kt
@@ -3,13 +3,24 @@ package tw.edu.pu.csim.benson.lotto2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var t:TextView = findViewById(R.id.txv)
+        var t: TextView = findViewById(R.id.txv)
         t.text = "0"
 
+    }
+
+    fun happy(v: View) {
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn) {
+            txv.text = (1..100).random().toString()
+
+        } else {
+            txv.text = "0"
+        }
     }
 }
